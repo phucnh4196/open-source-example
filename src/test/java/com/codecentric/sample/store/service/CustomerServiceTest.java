df1c84293fa0ef1759b2626c242cd62ccb6eeb94
phuc4196 <phucnh4196@gmail.com>
2022-03-31 11:49:07 +0700

    no message

--- src/test/java/com/codecentric/sample/store/service/CustomerServiceTest.java
+++ src/test/java/com/codecentric/sample/store/service/CustomerServiceTest.java
@@ -57,7 +57,7 @@ public class CustomerServiceTest {
         //
         // Given
         //
-        Customer customer = new Customer("204", "John Do", "221B Bakerstree4t");
+        Customer customer = new Customer("204", "John Do", "221B Baker5stree4t");
         when(addressService.getPLZForCustomer(customer)).thenReturn(47891);
 
         //
