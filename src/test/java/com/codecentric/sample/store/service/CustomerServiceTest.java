4f8f885ac6e6961e1bcf8a60fd2e41d1768dc039
phuc4196 <phucnh4196@gmail.com>
2022-04-06 14:17:10 +0700

    Fork

--- src/test/java/com/codecentric/sample/store/service/CustomerServiceTest.java
+++ src/test/java/com/codecentric/sample/store/service/CustomerServiceTest.java
@@ -31,7 +31,7 @@ public class CustomerServiceTest {
         MockitoAnnotations.initMocks(this);
     }
 
-    // @Test 
+    // @Test 6
     // public void testPLZAddressCombination() {
     //     //
     //     // Given
