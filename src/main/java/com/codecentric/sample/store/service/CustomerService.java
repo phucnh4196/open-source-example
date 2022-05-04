c089e25cf806f2fc0d8934c4e39dbfcfc39d4796
phucnh36 <95060651+phucnh36@users.noreply.github.com>
2022-05-04 11:48:48 +0700

    Update CustomerService.java

--- src/main/java/com/codecentric/sample/store/service/CustomerService.java
+++ src/main/java/com/codecentric/sample/store/service/CustomerService.java
@@ -17,7 +17,7 @@ public class CustomerService {
 
 
     public String getPLZAddressCombination(Customer customer) {
-
+        String r = "";
         String result = Integer.toString(addressService.getPLZForCustomer(customer))
                 + "_"
                 + addressService.getAddressForCustomer(customer);
